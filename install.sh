2224b585574b685749eedcf1a9b34a5d05b1db8f
Chris van der Scheer <chrisvanderscheer@MacBook-Air-van-Chris.local>
2022-05-10 20:15:06 +0200

    update installation setup

--- install.sh
+++ install.sh
@@ -5,7 +5,3 @@ cd wordpress-docker-compose
 # Use make command for automatic installation and
 # configuration of wordpress
 make autoinstall
-
-# Or, use docker-compose standard commands
-docker-compose up -d --build
-docker-compose -f docker-compose.yml -f wp-auto-config.yml run --rm wp-auto-config
